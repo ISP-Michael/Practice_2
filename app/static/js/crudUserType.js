41dbfb53b84e8a9be75ae15adf5c42d618951401
Michael <mtr.mikhail090905@gmail.com>
2025-01-09 02:38:53 +0300

    change indents in app/static/js/crud...

--- app/static/js/crudUserType.js
+++ app/static/js/crudUserType.js
@@ -13,120 +13,120 @@ async function getUserTypes() {
 
 async function getUserType(usertype_id) {
     const response = await fetch(`/utype/usertypes/${usertype_id}`, {
-		method: 'GET',
-		headers: {'Accept': 'application/json'}
-	});
+        method: 'GET',
+        headers: {'Accept': 'application/json'}
+    });
     if (response.ok === true) {
-		const usertype = await response.json();
-		document.getElementById('userTypeId').value = usertype.usertype_id;
-		document.getElementById('userType').value = usertype.usertype;
-	}
+        const usertype = await response.json();
+        document.getElementById('userTypeId').value = usertype.usertype_id;
+        document.getElementById('userType').value = usertype.usertype;
+    }
     else {
-		const error = await response.json();
-		console.log(error.message);
-	}
+        const error = await response.json();
+        console.log(error.message);
+    }
 }
 
 
 async function createUserType(userType) {
     const response = await fetch('/utype/addUserType', {
-		method: 'POST',
-		headers: {
-		  'Accept': 'application/json',
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-		  usertype: userType
-		})
-	});
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          usertype: userType
+        })
+    });
     if (response.ok === true) {
-		const usertype = await response.json();
-		document.querySelector('tbody').append(row(usertype));
+        const usertype = await response.json();
+        document.querySelector('tbody').append(row(usertype));
     }
     else {
-		const error = await response.json();
-		console.log(error.message);
+        const error = await response.json();
+        console.log(error.message);
     }
 }
 
 
 async function editUserType(userTypeId, userType) {
     const response = await fetch(`/utype/updateUserType/${userTypeId}`, {
-		method: 'PUT',
-		headers: {
-		  'Accept': 'application/json',
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-		  usertype: userType
-		})
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          usertype: userType
+        })
     });
     if (response.ok === true) {
-		const usertype = await response.json();
-		document.querySelector(`tr[data-rowid='${usertype.id}']`).replaceWith(row(usertype));
+        const usertype = await response.json();
+        document.querySelector(`tr[data-rowid='${usertype.id}']`).replaceWith(row(usertype));
     }
     else {
-		const error = await response.json();
-		console.log(error.message);
+        const error = await response.json();
+        console.log(error.message);
     }
 }
 
 
 async function deleteUserType(id) {
     const response = await fetch(`/utype/deleteUserType/${id}`, {
-		method: 'DELETE',
-		headers: {'Accept': 'application/json'}
+        method: 'DELETE',
+        headers: {'Accept': 'application/json'}
     });
     if (response.ok === true) {
-		const usertype = await response.json();
-		document.querySelector(`tr[data-rowid='${usertype.id}']`).remove();
+        const usertype = await response.json();
+        document.querySelector(`tr[data-rowid='${usertype.id}']`).remove();
     }
     else {
-		const error = await response.json();
-		console.log(error.message);
+        const error = await response.json();
+        console.log(error.message);
     }
 }
 
 
 function reset() {
-	document.getElementById('userTypeId').value = '';
-	document.getElementById('userType').value = '';
+    document.getElementById('userTypeId').value = '';
+    document.getElementById('userType').value = '';
 }
 
 
 function row(usertype) {
-	const tr = document.createElement('tr');
-	tr.setAttribute('data-rowid', usertype.id);
-	const usertypeTd = document.createElement('td');
-	usertypeTd.append(usertype.usertype);
-	tr.append(usertypeTd);
-	const linksTd = document.createElement('td');
-	const editLink = document.createElement('button');
-	editLink.append('Изменить');
-	editLink.addEventListener('click', () => {
-		document.getElementById('userTypeId').value = usertype.id;
-		document.getElementById('userType').value = usertype.usertype;
-	});
-	linksTd.append(editLink);
-	const removeLink = document.createElement('button');
-	removeLink.append('Удалить');
-	removeLink.addEventListener('click', async() => await deleteUserType(usertype.id));
-	linksTd.append(removeLink);
-	tr.appendChild(linksTd);
-	return tr;
+    const tr = document.createElement('tr');
+    tr.setAttribute('data-rowid', usertype.id);
+    const usertypeTd = document.createElement('td');
+    usertypeTd.append(usertype.usertype);
+    tr.append(usertypeTd);
+    const linksTd = document.createElement('td');
+    const editLink = document.createElement('button');
+    editLink.append('Изменить');
+    editLink.addEventListener('click', () => {
+            document.getElementById('userTypeId').value = usertype.id;
+            document.getElementById('userType').value = usertype.usertype;
+    });
+    linksTd.append(editLink);
+    const removeLink = document.createElement('button');
+    removeLink.append('Удалить');
+    removeLink.addEventListener('click', async() => await deleteUserType(usertype.id));
+    linksTd.append(removeLink);
+    tr.appendChild(linksTd);
+    return tr;
 }
 
 
 document.getElementById('resetBtn').addEventListener('click', () => reset());
 document.getElementById('saveBtn').addEventListener('click', async() => {
-	const id = document.getElementById('userTypeId').value;
+    const id = document.getElementById('userTypeId').value;
     const usertype = document.getElementById('userType').value;
-	if (id === '') {
-		await createUserType(usertype);
+    if (id === '') {
+        await createUserType(usertype);
     }
     else {
-		await editUserType(id, usertype);
-		reset();
+        await editUserType(id, usertype);
+        reset();
     }
 });
 
